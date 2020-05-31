2dd48692a268697e3e02dfa007fc76bfc83cc1c0
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-31 06:58:37 +0000

    [add]gem'bootstrap'とgem'jquery-rails'を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :salons
   devise_for :users
 
+  root "homes#top"
+  get 'about' => 'homes#about'
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
