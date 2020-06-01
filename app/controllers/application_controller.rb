a19e771cbb721659fdb3378ad373031869f65e1a
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-01 03:06:49 +0000

    [update]sign up keyを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:kana_name,:email,:postal_code,:address])
+   devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+   devise_parameter_sanitizer.permit(:account_update,keys:[:name,:kana_name,:email,:postal_code,:address])
+    #sign_upの際にnameのデータ操作を許。追加したカラム。
+   end
 end
