9fdff873dcac7ad00a61d71bf28c0e404ddfcbf0
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-02 03:10:58 +0000

    [update]sign upのkeyを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	protected
 
+	# 登録、ログイン、編集可能なkeyを指定
 	def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:kana_name,:email,:postal_code,:address])
-   devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
-   devise_parameter_sanitizer.permit(:account_update,keys:[:name,:kana_name,:email,:postal_code,:address])
-    #sign_upの際にnameのデータ操作を許。追加したカラム。
+		if resource_class == User
+   			devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:kana_name,:email,:postal_code,:address])
+   			devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+   			devise_parameter_sanitizer.permit(:account_update,keys:[:name,:kana_name,:email,:postal_code,:address])
+		elsif resource_class == Salon
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:salon_name,:name,:kana_name,:email,:postal_code,:address])
+   			devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+   			devise_parameter_sanitizer.permit(:account_update,keys:[:salon_name,:name,:kana_name,:email,:postal_code,:address])
+    	else
+    		super
+    	end
    end
 end
