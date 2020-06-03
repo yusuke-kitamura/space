fe772e1358306982fd430889736dadbf9a875dee
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-03 01:49:03 +0000

    [add]各ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,16 @@ Rails.application.routes.draw do
   	resource :users, only: [:show, :edit, :update]
   	resources :salons, only: [:index, :show]
   	resources :favorites, only: [:index, :show, :create, :destroy]
+  	resources :reservations, only: [:create, :show, :create, :destroy]
   end
 
   root "homes#top"
   get 'about' => 'homes#about'
 
-  namespace :salon do # urlの頭にsalon/を付与
+  namespace :owner do # urlの頭にowner/を付与
+  	resource :owners, only: [:show, :edit, :update, :destroy]
+  	resources :salons
+  	resources :reservations, only: [:index, :show]
   end
 
 end
