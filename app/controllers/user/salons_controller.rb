3088318c3e35dc8fe2ebb43625693e1f7d5dedd3
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-07 09:39:51 +0000

    [update]navタグのデザイン変更

--- app/controllers/user/salons_controller.rb
+++ app/controllers/user/salons_controller.rb
@@ -4,5 +4,11 @@ class User::SalonsController < ApplicationController
 	end
 
 	def show
+		@salon = Salon.find(params[:id])
+	end
+
+	private
+	def salon_params
+		params.require(:salon).permit(:salon_name, :salon_image, :explanation, :postal_code, :address, :price, :seat_number)
 	end
 end
