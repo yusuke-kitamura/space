687e795402b4abb8ab4d01fabae46812a1c79de2
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-09 14:52:38 +0000

    [add]お気に入り解除ボタン作成

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -11,6 +11,11 @@ class User::FavoritesController < ApplicationController
 		@favorites = Favorite.where(user_id: @user.id).all
 	end
 
-	def destory
+	def destroy
+		user = current_user
+		salon = Salon.find(params[:salon_id])
+		@favorite = Favorite.find_by(user_id: user.id, salon_id: salon.id)
+		@favorite.destroy
+		redirect_back(fallback_location: root_path)
 	end
 end
