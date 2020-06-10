9f47694025e60b242c111a50ca28f9130ccdfc57
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-10 08:48:22 +0000

    [add]ストロングパラメーターを追加

--- app/controllers/user/reservations_controller.rb
+++ app/controllers/user/reservations_controller.rb
@@ -1,13 +1,19 @@
 class User::ReservationsController < ApplicationController
 
 	def new
+		@reservation = Reservation.new
 	end
-	
+
 	def create
 		@reservation = current_user.reservations.create(reservation_params)
 		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
+	end
+
+	private
+	def reservation_params
+		params.require(:reservation).permit(:salon_id, :user_id, :date)
 	end
 end
