8e4d7707fc883946283b632b2fcbba765afcad12
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-20 15:17:14 +0000

    [add]pw変更メール送信用の記述を追加

---
+++ db/migrate/20200620081159_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :confirmation_token, :string
+  	add_column :users, :confirmed_at, :datetime
+  	add_column :users, :confirmation_sent_at, :datetime
+  	add_column :users, :cunconfirmed_email, :string
+  end
+end
