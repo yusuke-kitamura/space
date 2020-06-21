97c13571337b7292e49a2e3de14a87a97adbde3d
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-21 06:50:20 +0000

    [add]smtpサーバーを設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,17 +60,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # デフォルトのurlを設定
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # pw変更に必要なsmtpなどの設定
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-    # :address => "smtp.gmail.com",
-    # :port => 587,
-    # :user_name => "Gmailアドレス",
-    # :password => "Gmailパスワード",
-    # :authentication => :plain,
-    # :enable_starttls_auto => true
-  # }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "yusuke.kitamura1209@gmail.com",
+    :password => "rdvppldstipomlpf",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
