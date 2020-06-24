b00c3f5b7a47c198e6b9dfe458e46dcc940abfba
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-24 12:08:24 +0000

    [update]空白、過去の日付は選択できないように実装

--- app/controllers/user/reservations_controller.rb
+++ app/controllers/user/reservations_controller.rb
@@ -2,15 +2,16 @@ class User::ReservationsController < ApplicationController
 	def create
 		@reservation = Reservation.new(reservation_params)
 		@reservation.user_id = current_user.id
-		if @reservation.save
-			redirect_to user_reservations_path, notice: "予約完了しました。"
-		elsif @reservation.date < Date.today
+
+		if  @reservation.date.nil?
+			@salon = Salon.find(params[:salon_id])
+			redirect_to user_salon_path(@salon), alert: "もう一度やり直してください"
+		elsif @reservation.date < DateTime.now
       		@salon = Salon.find(params[:salon_id])
       		redirect_to user_salon_path(@salon), alert: "過去の日付は選択できません"
 		else
-			@salon = Salon.find(params[:salon_id])
-			redirect_to user_salon_path(@salon), alert: "もう一度やり直してください"
-			
+			@reservation.save
+			redirect_to user_reservations_path, notice: "予約完了しました。"
 		end
 	end
 
