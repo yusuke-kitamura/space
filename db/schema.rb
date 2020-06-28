bb246bcd51261b913f7882bcbf15a0a1a3e55513
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-06-28 05:28:40 +0000

    [update]本番環境エラーのためphone_numberのデータ型変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_160249) do
+ActiveRecord::Schema.define(version: 2020_06_28_051811) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_160249) do
     t.string "kana_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
