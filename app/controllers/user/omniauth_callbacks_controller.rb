78d91d7b80186eb9fd50ba47b4f98e8f8b5c9c34
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-08-21 05:38:05 +0000

    [remove]controller for login with line app

--- app/controllers/user/omniauth_callbacks_controller.rb
+++
@@ -1,2 +0,0 @@
-class User::OmniauthCallbacksController < ApplicationController
-end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,4 +27,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
 end
